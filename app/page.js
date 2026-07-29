84e5309637c78ebab7ae2a4caf38edfd65715473
Sakib hasan <sakibhasan16h@gmail.com>
2026-07-29 07:06:17 +0600

    first commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Welcome to My Next.js App</h1>
+    </div>
+  );
+}
